26b0ce4b9b2f25cd1a2cad5b7a6938dd81dd7552
Kfir Gollan <kfir.gollan@gmail.com>
2017-09-30 12:11:54 +0300

    Add python3.6 installation script.

---
+++ scripts/install_python3.6.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Taken from: https://askubuntu.com/questions/865554/how-do-i-install-python-3-6-using-apt-get
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6
