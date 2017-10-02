eb7c3d3fdb4c9f5b070af7dca6ea022017cd8a2c
Kfir Gollan <kfir.gollan@gmail.com>
2017-10-02 13:17:15 +0300

    Add cscope_gen script

---
+++ cscope_gen.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+dir=$2
+
+echo "Locating files..."
+src_files=$(find ${dir} -name '*.c' -o -name "*.h" | xargs realpath)
+echo ${src_files} | tr " " "\n" > cscope.files
+
+echo "Rebuilding cscope db..."
+# -b: just build
+# -q: create inverted index
+cscope -b -q
+
+echo "Rebuilding ctags db..."
+
+# update ctags as well
+etags ${src_files}
