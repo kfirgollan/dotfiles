e5c9327fb8af4461839bbb8ddfc3494355bbfaf5
Kfir Gollan <kfir@drivenets.com>
2018-10-13 12:20:07 +0000

    Add git lg alias for viewing logs

---
+++ bash/git_config.sh
@@ -0,0 +1,2 @@
+# Configure git lg as a log alias
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
